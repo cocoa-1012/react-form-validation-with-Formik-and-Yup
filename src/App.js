0d7847897f298e0c87c2dc58b825e02b375f5cef
cocoa-1012 <cocoa.pandaok@gmail.com>
2021-11-15 21:58:19 -0500

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,136 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import React from "react";
+import * as Yup from "yup";
+import "./App.css";
 
 function App() {
+  const validationSchema = () => {
+    return Yup.object().shape({
+      fullname: Yup.string().required("Fullname is required"),
+      username: Yup.string()
+        .required("Username is required")
+        .min(6, "Username must be at least 6 characters")
+        .max(20, "Username must be exceed 20 characters"),
+      email: Yup.string()
+        .required("Email is required")
+        .email("Email is inivalid"),
+      password: Yup.string()
+        .required("Password is required")
+        .min(6, "Password must be at least 6 characters")
+        .max("Password must not exceed 40 characters"),
+      confirmPassword: Yup.string()
+        .required("Confirm Password is required")
+        .oneOf([Yup.ref("password"), null], "Confirm Pasword does not match"),
+      acceptTerms: Yup.bool().oneOf([true], "Accept Terms is required"),
+    });
+  };
+  const handleSubmit = (data) => {
+    console.log(JSON.stringify(data, null, 2));
+  };
+  const initialValues = {
+    fullname: "",
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    acceptTerms: false,
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="register-form">
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
         >
-          Learn React
-        </a>
-      </header>
+          {({ resetForm }) => (
+            <Form>
+              <div className="form-group">
+                <label htmlFor="fullname">Full Name</label>
+                <Field name="fullname" type="text" className="form-control" />
+                <ErrorMessage
+                  name="fullname"
+                  component="div"
+                  className="text-danger"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="username">User Name</label>
+                <Field name="username" type="text" className="form-control" />
+                <ErrorMessage
+                  name="username"
+                  component="div"
+                  className="text-danger"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <Field name="email" type="email" className="form-control" />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="text-danger"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <Field
+                  name="password"
+                  type="password"
+                  className="form-control"
+                />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="text-danger"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Confirm Password</label>
+                <Field
+                  name="confirmPassword"
+                  type="password"
+                  className="form-control"
+                />
+                <ErrorMessage
+                  name="confirmPassword"
+                  component="div"
+                  className="text-danger"
+                />
+              </div>
+              <div className="form-group form-check">
+                <Field
+                  name="acceptTerms"
+                  type="checkbox"
+                  className="form-check-input"
+                />
+                <label htmlFor="acceptTerms" className="form-check-label">
+                  {" "}
+                  I have read and agree to the Terms
+                </label>
+                <ErrorMessage
+                  name="acceptTerms"
+                  component="div"
+                  className="text-danger"
+                />
+              </div>
+              <div className="form-group">
+                <button type="submit" className="btn btn-primary">
+                  Register
+                </button>
+                <button
+                  type="button"
+                  onClick={resetForm}
+                  className="btn btn-warning float-right"
+                >
+                  Reset
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 }
